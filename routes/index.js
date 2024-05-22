eb9514e852b3a2451c7e32d90a834b11b4512c84
darianwwu <103262720+darianwwu@users.noreply.github.com>
2024-05-22 15:37:45 +0200

    Clean up ignored files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Citizen Science - Lärmkartierung Münster' });
+  res.render('index', { title: 'Semi-automatic detection of fake satellite imagery' });
 });
 
 module.exports = router;
