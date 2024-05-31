47b79ee3cc19b89b95e8e566dfb87d7c54b325da
darianwwu <103262720+darianwwu@users.noreply.github.com>
2024-05-31 17:19:09 +0200

    Erste Probeversion von Geolocating hinzugefügt

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,43 @@
 // JavaScript-Code in der script.js
 const imageUpload = document.getElementById('imageUpload');
 const previewImage = document.getElementById('previewImage');
+const buttonLocation = document.getElementById('locationInputApply');
+const feldLocation = document.getElementById('locationInput');
 
-imageUpload.addEventListener('change', function() {
-    const file = this.files[0];
-    const reader = new FileReader();
+// Initialisierung der Karte
+var map = L.map('map').setView([-41.2858, 174.78682], 14);
 
-    reader.addEventListener('load', function() {
-        previewImage.src = reader.result;
-    });
+// Hinzufügen der Tile Layer
+var satelliteLayer = L.tileLayer(
+  'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; <a href="http://www.esri.com/">Esri</a>, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxZoom: 18,
+}).addTo(map);
 
-    if (file) {
-        reader.readAsDataURL(file);
-    }
+var labelLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}.png', {
+  minZoom: 0,
+  maxZoom: 20,
+  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  ext: 'png'
 });
+
+var baseMaps = {
+  "OpenStreetMap": L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }),
+  "Satellite": satelliteLayer
+};
+
+var overlayMaps = {
+  "Labels": labelLayer
+};
+
+L.Control.geocoder().addTo(map);
+
+// Hinzufügen des Satellite Layers und des Label Layers
+satelliteLayer.addTo(map);
+labelLayer.addTo(map);
+
+// Hinzufügen der Layer Controls
+L.control.layers(baseMaps, overlayMaps).addTo(map);
