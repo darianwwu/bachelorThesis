5ca3b596025a8a2f27f5d00e9f57fcd43976cdd9
darianwwu <103262720+darianwwu@users.noreply.github.com>
2024-07-03 11:26:34 +0200

    Neue Funktionen

--- public/js/script.js
+++ public/js/script.js
@@ -8,15 +8,43 @@ const bildTransaparenzRegler = document.getElementById('transparenzRegler');
 const bildDuplikatTransparenz = document.getElementById('transparentesOverlay');
 const tifanzeige = document.getElementById('my-img');
 const textNLPButton = document.getElementById('textInputApply');
+const kartenCoordsButton = document.getElementById('kartenCoordsApply');
 
 let coordinates = { lat: 52.96251, lng: 17.625188 }; //Test-Koordinaten, werden später durch dynamische Koordinaten ersetzt
+let mapcoordinates = {minLng: 0, minLat: 0, maxLng: 0, maxLat: 0};
 let inputtext = "Das Atomium in Brüssel Belgien ist ein sehr interessantes Bauwerk, hier its ien Bild davon.";
 let ueberlagert = false;
 let entitaetenarray = [];
 
-// Initialisierung der Karte
-var map = L.map('map').setView([-41.2858, 174.78682], 14);
+kartenCoordsButton.addEventListener('click', () => {
+  fetch('http://localhost:5000/imagefrommap', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(mapcoordinates),
+  })
+  .then(response => response.blob())
+  .then(blob => {
+    const url = URL.createObjectURL(blob);
+    const tifanzeige = document.getElementById('my-img');
+    tifanzeige.src = url;
+    console.log(blob);
+    //let durchschnittLat = (mapcoordinates.maxLat + mapcoordinates.minLat) /2;
+    //let durchschnittLng = (mapcoordinates.maxLng + mapcoordinates.minLng) /2;
+    //let differenceLat = mapcoordinates.maxLat - mapcoordinates.minLat;
+    //let differenceLng = mapcoordinates.maxLng - mapcoordinates.minLng;
+    let eckenkoordinaten = L.latLngBounds(L.latLng(mapcoordinates.minLat, mapcoordinates.minLng), L.latLng(mapcoordinates.maxLat, mapcoordinates.maxLng));
+    map.fitBounds(eckenkoordinaten);
+    //console.log("durchschnittLat: ",durchschnittLat, " durchschnittLng: ",durchschnittLng, " differenceLat: ",differenceLat, " differenceLng: ",differenceLng);
+    drawnItems.clearLayers();
+  })
+  .catch((error) => console.error('Error:', error));
+  });
 
+/**
+ * Event-Listener, der die NLP Analyse des Textes mit Klicken auf den entsprechenden Button ausführt.
+ */
 textNLPButton.addEventListener('click', () => {
   fetch('http://localhost:5000/classify', {
     method: 'POST',
@@ -51,8 +79,6 @@ textNLPButton.addEventListener('click', () => {
 
     // Entfernen der Artikel
     textForGeocode = removeArticles(textForGeocode);
-
-    // Logge den zusammengestellten Text
     console.log('Text for Geocode:', textForGeocode);
   
     // Führe die POST-Anfrage mit dem zusammengestellten Text aus
@@ -69,13 +95,19 @@ textNLPButton.addEventListener('click', () => {
   .then(response => response.json())
   .then(data => {
     // Verarbeite die Antwort der /geocode Route
-    console.log('Geocode Data:', data);
+    //console.log('Coordinates vor Änderung:', coordinates);
+    //console.log('Geocode Data:', data);
+    coordinates= { lat: parseFloat(data.coords.lat), lng: parseFloat(data.coords.lon) };
+    console.log('Coordinates nach Änderung:', coordinates);
   })
   .catch((error) => {
     console.error('Error:', error);
   });
 });
 
+/**
+ * Event-Listener, der das Bild über die Karte legt, wenn der Button geklickt wird.
+ */
 bildUeberKarteButton.addEventListener('click', () => {
   if(ueberlagert == false) {
     bildDuplikatTransparenz.src = previewImage.src;
@@ -91,12 +123,15 @@ bildUeberKarteButton.addEventListener('click', () => {
   }
 });
 
+/**
+ * Event-Listener, der die Transparenz des über die Karte gelegten Bildes ändert.
+ */
 bildTransaparenzRegler.addEventListener('change', () => {
   console.log(bildTransaparenzRegler.value);
   bildDuplikatTransparenz.style.opacity = bildTransaparenzRegler.value /100;
 });
 /**
- * Event-Listener, ...
+ * Event-Listener, der die Koordinaten an den python Server schickt und ein Earth Engine Bild erstellt und anzeigt.
  */
 buttonLocation.addEventListener('click', () => {
   fetch('http://localhost:5000/image', {
@@ -134,8 +169,8 @@ imageUpload.addEventListener('change', function() {
     }
 });
 
-
-
+// Initialisierung der Karte
+var map = L.map('map').setView([51.505, -0.09], 13);
 // Hinzufügen der Tile Layer, gefunden unter https://leaflet-extras.github.io/leaflet-providers/preview/
 var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZHdlaXNzd3d1IiwiYSI6ImNsd3oxN2g5dDAyeGwycHF1Z29mYjV5enUifQ.7PQUPuJn6Nzz_tXGsIWdUw', {
 	minZoom: 0,
@@ -172,6 +207,45 @@ L.simpleMapScreenshoter().addTo(map);
 // Hinzufügen der Layer Controls
 L.control.layers(baseMaps, overlayMaps).addTo(map);
 
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+  draw: {
+    polyline: false,
+    polygon: false,
+    circle: false,
+    marker: false,
+    circlemarker: false,
+    rectangle: true
+  },
+  edit: {
+    featureGroup: drawnItems
+  }
+});
+map.addControl(drawControl);
+
+map.on('draw:created', function (e) {
+  var type = e.layerType,
+    layer = e.layer;
+
+  drawnItems.addLayer(layer);
+
+  if (type === 'rectangle') {
+    var bounds = layer.getBounds();
+    //var topLeft = bounds.getNorthWest();
+    var topRight = bounds.getNorthEast();
+    var bottomLeft = bounds.getSouthWest();
+    //var bottomRight = bounds.getSouthEast();
+    mapcoordinates = {minLng: bottomLeft.lng, minLat: bottomLeft.lat, maxLng: topRight.lng, maxLat: topRight.lat};
+    console.log(mapcoordinates);
+  }
+});
+
+/**
+ * Ließt die Koordinaten aus den Eingabefeldern aus und gibt sie als Objekt zurück.
+ * @returns Kombinierte Koordinaten aus den Eingabefeldern
+ */
 function koordinatenAuslesen() {
   let coordinateLat;
   let coordinateLon;
@@ -188,6 +262,11 @@ function koordinatenAuslesen() {
   }
 }
 
+/**
+ * Entfernt Artikel aus einem Text. Artikel werden nur entfernt, wenn sie alleine stehen (mit Leerzeichen dahinter), das Leerzeichen wird mit entfernt.
+ * @param {String} text Text, aus dem die Artikel entfernt werden sollen
+ * @returns Text ohne Artikel
+ */
 function removeArticles(text) {
   const articles = ["der ", "die ", "das ", "ein ", "eine ", "eines ", "einer ", "einem ", "den ", "dem ", "des ", "the ", 
                     "Der ", "Die ", "Das ", "Ein ", "Eine ", "Eines ", "Einer ", "Einem ", "Den ", "Dem ", "Des ", "The "];
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 from osgeo import gdal
+from skimage import io, filters, feature
+import matplotlib.pyplot as plt
+import cv2
 import ee
 import os
 import shutil
@@ -10,7 +13,11 @@ import subprocess
 import time
 import spacy
 import geocoder
+import tweepy
 from google.oauth2.service_account import Credentials
+from PIL import Image, ImageChops, ImageEnhance
+import numpy as np
+from collections import Counter
 
 app = Flask(__name__)
 CORS(app)
@@ -20,6 +27,15 @@ key_file = './credentials/ee-heinich04-805b2e12705e.json'
 scopes = ['https://www.googleapis.com/auth/earthengine']
 #api_key = 'AIzaSyB5OacCI7Nt76RIUn0qeyoGMKhFBdEWUaU'
 
+twitter_api_key = '4TzCQMeLfu6kClXdUEwlhlWwd'
+twitter_api_key_secret = 'QlpUT15BywcD0PjQigRquuKSnZMJy64rHm0F8u1u56hl6WMvht'
+twitter_bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEYWugEAAAAATZ5pNTl0naZyvuvMDQFb1bkvduU%3DlDs8iHTEy7AohL8jPb19f5p1p2VErEwu6KDZKF3Nk0D7zOOGx1'
+twitter_access_token = '1670759875285065729-S0ltHACDHfG3DD5aELrpkL2BeQd5ZN'
+twitter_access_token_secret = '30VTA2g7WGurD70QtxggpAWqkoLfSfmA1nt8Ee4q35zP0'
+
+auth = tweepy.OAuth1UserHandler(twitter_api_key, twitter_api_key_secret, twitter_access_token, twitter_access_token_secret)
+api = tweepy.API(auth)
+
 # Authentifizierung mit JWT
 credentials = Credentials.from_service_account_file(key_file, scopes=scopes)
 
@@ -28,6 +44,21 @@ ee.Initialize(credentials)
 
 # Trainiertes Modell laden
 nlp = spacy.load(r"C:\Users\User\Documents\GitHub\bachelorThesis\trainiertesmodell")
+
+
+def tweet_analysieren():
+    tweet_id = '1800815302608838722'
+    tweet = api.get_status(tweet_id)
+
+    twitter_text = tweet.text
+    media = tweet.entities.get('media', [])
+    if len(media) > 0:
+        image_url = media[0]['media_url']
+
+    print(f'Text: {twitter_text}')
+    print(f'Image URL: {image_url}')
+
+#tweet_analysieren()
 
 @app.route('/')
 def hello_world():
@@ -94,6 +125,140 @@ def classify_text():
         app.logger.exception(e)
         return str(e), 500
 
+@app.route('/imagefrommap', methods=['POST'])
+def get_image_from_map():
+    try:
+        # Koordinaten und Parameter festlegen
+        mapcoordinates = request.get_json()
+        coords = ee.Geometry.Rectangle([mapcoordinates['minLng'], mapcoordinates['minLat'], mapcoordinates['maxLng'], mapcoordinates['maxLat']])
+        maxCloudCover = 30
+        #bufferSize = 4000
+        # Die Sentinel-2-Bildsammlung laden
+        sentinel2 = (ee.ImageCollection('COPERNICUS/S2')
+            .filterBounds(coords)
+            .filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', maxCloudCover))
+            .sort('system:time_start', False))
+
+        # Das neueste Bild auswählen
+        #TODO: Zeitlichen Aspekt berücksichtigen
+        image = sentinel2.first()
+
+        # Ein rechteckiges Gebiet um den Punkt erstellen
+        region = coords.bounds()
+
+        # RGB-Bänder auswählen und auf das rechteckige Gebiet zuschneiden
+        rgbImage = image.select(['B4', 'B3', 'B2']).clip(region)
+
+        # Eine Download-URL für das Bild generieren
+        url = rgbImage.getDownloadUrl({
+            'scale': 10,
+            'crs': 'EPSG:4326',
+            'region': region.getInfo()['coordinates'],
+            'fileFormat': 'GeoTIFF'
+        })
+        
+        # Das Bild herunterladen
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception(f"Request failed with status {response.status_code}")
+
+        # Die ZIP-Datei als temporäre Datei speichern
+        zip_file_name = 'image.zip'
+        with open(zip_file_name, 'wb') as f:
+            f.write(response.content)
+
+        # Die ZIP-Datei entpacken
+        with zipfile.ZipFile(zip_file_name, 'r') as zip_ref:
+            zip_ref.extractall('.')
+
+        # Die TIF-Dateien durchlaufen und verschieben
+        tif_files = []
+        tifs_directory = 'C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs'
+
+        if not os.path.exists(tifs_directory):
+            os.makedirs(tifs_directory)
+
+        for tif_file_name in os.listdir('.'):
+            if tif_file_name.endswith('.tif'):
+                current_path = os.path.join(os.getcwd(), tif_file_name)
+                band_name = tif_file_name.split('.')[-2].split('_')[-1]
+                new_file_name = f"{band_name}.tif"
+                new_path = os.path.join(tifs_directory, new_file_name)
+                shutil.move(current_path, new_path)
+
+        # Die TIF-Dateien zu einer einzigen Datei zusammenführen
+        python_exe = 'C:\\Users\\User\\anaconda3\\python.exe'
+        gdal_merge = 'C:\\Users\\User\\anaconda3\\Scripts\\gdal_merge.py'
+        output_file = 'C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\merged.tif'
+        tif_files = ['C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\B4.tif', 
+                    'C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\B3.tif', 
+                    'C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\B2.tif']
+
+        command = [python_exe, gdal_merge, '-init', '255', '-o', output_file, '-separate'] + tif_files
+
+        try:
+            result = subprocess.run(command, shell=False, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print("Ausgabe:", result.stdout.decode())
+            print("Fehlerausgabe:", result.stderr.decode())
+        except subprocess.CalledProcessError as e:
+            print("Fehler beim Ausführen von gdal_merge:", e)
+            print("Befehl:", ' '.join(command))
+            print("Fehlerausgabe:", e.stderr.decode())
+            raise
+
+        # Warten, bis die Datei existiert und ihre Größe sich nicht mehr ändert
+        old_file_size = 0
+        while not os.path.isfile(output_file) or old_file_size != os.path.getsize(output_file):
+            print("Warten auf die Erstellung der Datei...")
+            old_file_size = os.path.getsize(output_file) if os.path.isfile(output_file) else 0
+            time.sleep(1)  # Warten Sie 1 Sekunde zwischen den Überprüfungen
+
+        # Das TIF in PNG umwandeln mithilfe von gdal_translate
+        gdal_translate = 'C:\\Program Files\\GDAL\\gdal_translate.exe'
+        input_file_new = 'C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\merged.tif'
+
+        # Basis-Output-Dateiname
+        output_file_base = 'C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\mergedtif.png'
+
+        # Überprüfen, ob die Datei bereits existiert
+        if os.path.exists(output_file_base):
+            # Falls die Datei existiert, eine neue Nummer suchen
+            count = 1
+            while True:
+                # Neuen Dateinamen erstellen
+                output_file_new = f'{output_file_base[:-4]}{count}.png'
+                if not os.path.exists(output_file_new):
+                    break
+                count += 1
+        else:
+            # Falls die Datei nicht existiert, den ursprünglichen Dateinamen verwenden
+            output_file_new = output_file_base
+        
+        command = [gdal_translate, '-ot', 'Byte', '-scale', '0', '3000', '0', '255', '-of', 'PNG', '-b', '1', '-b', '2', '-b', '3', input_file_new, output_file_new]
+
+        try:
+            result = subprocess.run(command, shell=False, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print("Ausgabe:", result.stdout.decode())
+            print("Fehlerausgabe:", result.stderr.decode())
+        except subprocess.CalledProcessError as e:
+            print("Fehler beim Ausführen von gdal_translate:", e)
+            print("Befehl:", ' '.join(command))
+            print("Fehlerausgabe:", e.stderr.decode())
+            raise
+
+        # Nicht mehr benötigte Dateien löschen
+        os.remove(zip_file_name)
+        os.remove('C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\B4.tif')
+        os.remove('C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\B3.tif')
+        os.remove('C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\B2.tif')
+        os.remove('C:\\Users\\User\\Documents\\GitHub\\bachelorThesis\\public\\tifs\\merged.tif')
+
+        # Das PNG-Bild zurückgeben
+        return send_file(output_file_new, mimetype='image/png')
+    except Exception as e:
+        app.logger.exception(e)
+        return str(e), 500
+    
 #Hauptroute, die in script.js mit Koordinaten aufgerufen wird und ein TIF und ein PNG der entsprechenden Region erstellt
 @app.route('/image', methods=['POST'])
 def get_image():
@@ -232,3 +397,5 @@ def get_image():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
