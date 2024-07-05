e1f4c41abd2e9c4d4230152e630b1487b62ca801
darianwwu <103262720+darianwwu@users.noreply.github.com>
2024-07-05 15:06:02 +0200

    image detector added

--- run_server.py
+++ run_server.py
@@ -1,5 +1,6 @@
 from waitress import serve
-from server import app
+from server import app, main
 
 if __name__ == '__main__':
+    main()  # Hauptskript von server.py ausführen
     serve(app, host='127.0.0.1', port=5000, threads=4)
